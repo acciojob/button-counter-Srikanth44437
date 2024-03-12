c024084e30912e2a057187cbedf17b22962b1df9
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-03-12 10:13:50 +0000

    click count

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 
 import React from "react";
 import './../styles/App.css';
+import { Component } from "react/cjs/react.production.min";
 
-const App = () => {
+class Counter extends Component{
+  constructor(props){
+    super(props);
+    this.state = {count :0};
+  }
+
+render(){
+
+  const incrementClick = () => {
+    const current = this.state.count;
+    this.setState({count : 1 + current})
+
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <>
+      <p>Button clicked {this.state.count} times</p>
+      <button onClick={incrementClick}>Click me</button>
+    </>
   )
 }
+}
 
-export default App
+
+export default Counter;
